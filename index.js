bfd0d383d48abd3656b2f800757bb43663d5c231
antieatingactivist <antieatingactivist@gmail.com>
2022-01-31 16:45:41 -0800

    added boilerplate code

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      user: 'root',
+      port: 3306,
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+
+//   db.query('SELECT * FROM employees', function (err, results) {
+//     console.log(err);
+//     console.log(results);
+//   });
+  
+  app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
