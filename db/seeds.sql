455f96d474fb318a3c28603beaa1a09fc1fb7af0
antieatingactivist <antieatingactivist@gmail.com>
2022-01-31 19:22:55 -0800

    added schemas

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,21 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jean-luc", "Picard", 2, 1),
-       ("William", "Riker", 3, 1),
-       ("Geordie", "LaForge", 4, 2),
-       ("Beverly", "Crusher", 5, 2),
-       ("Miles", "Obrien", 6, 3);
+INSERT INTO department (id, name)
+VALUES  (1, "Command"),
+        (2, "Science"),
+        (3, "Engineering"),
+        (4, "Security"),
+        (5, "Medical");
+
+INSERT INTO role (id, name)
+VALUES  (1, "Captain"),
+        (2, "First Officer"),
+        (3, "Head Engineer"),
+        (4, "Doctor"),
+        (5, "Transporter Operator");
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("Jean-luc", "Picard" , 1),
+       ("William", "Riker", 1),
+       ("Geordie", "LaForge", 2),
+       ("Beverly", "Crusher", 2),
+       ("Miles", "Obrien", 3);
        
