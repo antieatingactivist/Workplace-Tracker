67272a475d2c5a34b628c5ef051fcb6ded234171
antieatingactivist <antieatingactivist@gmail.com>
2022-01-31 21:22:54 -0800

    completed schemas

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,18 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL;
-    department_id INT NOT NULL
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
@@ -20,6 +24,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role (id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee (id)
 
 );
